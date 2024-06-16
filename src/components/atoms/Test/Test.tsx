1915a8aa24830b064f9d5b024eb8a8e91d802009
Just-a-developer-abhi <abhi344741@gmail.com>
2024-06-16 15:42:56 +0530

    Testing disabled workflow of dev chromatic

--- src/components/atoms/Test/Test.tsx
+++ src/components/atoms/Test/Test.tsx
@@ -2,9 +2,6 @@ const Test = ({ label }: { label: string }) => {
 	return (
 		<div className='px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded hover:bg-blue-700'>
 			{label}
-			<div>
-				<p className='text-sm text-gray-500'> Just Checking</p>
-			</div>
 		</div>
 	);
 };
